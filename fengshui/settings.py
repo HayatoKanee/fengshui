415055872d9e249f57c22b5ccd1f5d70cfbf0039
hayatokane <cheungkuensum@gmail.com>
2025-05-22 04:56:56 +0100

    Change requirements.txt

--- fengshui/settings.py
+++ fengshui/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,11 +72,13 @@ WSGI_APPLICATION = 'fengshui.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",   # fallback for local dev
+        conn_max_age=600,
+        ssl_require=True                   # enforce SSL in production
+    )
 }
 
 # Password validation
